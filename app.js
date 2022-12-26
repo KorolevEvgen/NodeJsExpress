83da31f0f77e3fd382a4061330a91a533c95790e
Jenya Korolev <korolev.evgen163@gmail.com>
2022-12-26 11:18:30 +0200

    socketIO

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const http = require('http');
+const socketIO = require('socket.io');
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const mongoose = require('mongoose');
@@ -10,13 +12,57 @@ const { cronRunner } = require('./cron');
 const swaggerJson = require('./swagger.json');
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const io = socketIO(server, { cors: 'http://localhost:80' });
+
+io.on('connection', (socket) => {
+
+    socket.on('disconnect', (socket) => {
+        console.log(`Socket ${socket.id} was disconnected`);
+    });
+
+    // console.log(socket.id);
+    //
+    // console.log(socket.handshake.auth);
+    // console.log(socket.handshake.query);
+
+    socket.on('message:send', (messageData) => {
+        console.log(messageData.text);
+
+        //SEND ONE TO ONE EVENT
+        // socket.emit('message:new',messageData.text)
+
+        //SEND EVENT TO ALL EXCEPT EMITER
+        // socket.broadcast.emit('message:new',messageData.text)
+
+        //SEND EVENT TO ALL CLIENTS
+        io.emit('message:new', messageData.text);
+
+
+    });
+
+    socket.on('room:join', (roomInfo) => {
+        socket.join(roomInfo.roomId); // SOCKET JOIN ROOM
+        // socket.leave(roomInfo.roomId); // SOCKET LEAVE ROOM
+
+        // SEND TO ALL IN ROOM EXCEPT NEW MEMBER
+        // socket.to(roomInfo.roomId).emit('user:room:join',socket.id)
+
+        // SEND TO ALL ROOM MEMBERS
+        io.to(roomInfo.roomId).emit('user:room:join', socket.id);
+    });
+});
+
+
+
+
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
-app.use('/docs', swaggerUI.serve,swaggerUI.setup(swaggerJson));
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJson));
 
 app.get('/', (req, res) => {
     res.json('WElCOME');
@@ -29,7 +75,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(configs.PORT, async () => {
+server.listen(configs.PORT, async () => {
     await mongoose.connect(configs.MONGO_URL);
     console.log(`Server listen ${configs.PORT}`);
     // cronRunner();
